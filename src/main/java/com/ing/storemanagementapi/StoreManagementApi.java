f2bdd84cbf449b6bad163c2cb41e1b91e6f2ee76
Buga Daniel <bugadanielmarian@gmail.com>
2024-05-14 14:43:13 +0300

    first commit

---
+++ src/main/java/com/ing/storemanagementapi/StoreManagementApi.java
@@ -0,0 +1,13 @@
+package com.ing.storemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementApi.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/storemanagementapi/StoreManagementApiTests.java
@@ -0,0 +1,13 @@
+package com.ing.storemanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApiTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
