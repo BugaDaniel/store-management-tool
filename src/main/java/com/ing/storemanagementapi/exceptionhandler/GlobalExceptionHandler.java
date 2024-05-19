b25feb87376a8857869d4c4b2f1518b116735231
Buga Daniel <bugadanielmarian@gmail.com>
2024-05-19 20:50:02 +0300

    add global exception handler

---
+++ src/main/java/com/ing/storemanagementapi/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.ing.storemanagementapi.exceptionhandler;
+
+import com.ing.storemanagementapi.exception.ProductInsertionViolationException;
+import com.ing.storemanagementapi.exception.ProductNotFoundException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ProductInsertionViolationException.class)
+    public ResponseEntity<String> handleTryToInsertProductWithIdException(ProductInsertionViolationException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> handleDataAccessException(DataAccessException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred while trying to access data base: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred: " + ex.getMessage());
+    }
+}
