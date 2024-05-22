a6b8ef26be168734f98124b8952a107b5d16d1ec
Buga Daniel <bugadanielmarian@gmail.com>
2024-05-22 14:14:49 +0300

    add concurrency for updateProduct method in service

--- src/main/java/com/ing/storemanagementapi/service/ProductService.java
+++ src/main/java/com/ing/storemanagementapi/service/ProductService.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -42,6 +44,7 @@ public class ProductService {
         return productRepository.save(product);
     }
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Product updateProduct(Long id, Product updatedProduct) {
         Product product = findProductById(id);
         updateAllProductFields(product, updatedProduct);
